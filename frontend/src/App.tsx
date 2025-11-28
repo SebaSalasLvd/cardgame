8862a6b0ee33264f13c2e06529b715d8f36ff08b
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-11-28 03:43:38 -0300

    feat(frontend): added router provider, specified routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {MainDisplay} from './components/MainDisplay'
+import CardSection from './components/CardSection'
+import { CardDetail } from './components/CardDetail'
+import { About } from './components/About'
 
 function App() {
-
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainDisplay />} />
+        <Route path="/card-section" element={<CardSection />} />
+        <Route path="/card-detail" element={<CardDetail />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
